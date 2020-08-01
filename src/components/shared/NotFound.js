71541fd3a7db2fd2404e6640f5051d68d02e75e9
taseenb <esteban.almiron@gmail.com>
2020-08-01 16:49:45 -0300

    404 page style

---
+++ src/components/shared/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NotFound () {
+  return (
+    <div className='not-found'>
+      <div>
+        <h1>404</h1>
+        <h2>Page not found</h2>
+      </div>
+    </div>
+  )
+}
