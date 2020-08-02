84ec69669e739065ed6c77751524299daeac572d
taseenb <esteban.almiron@gmail.com>
2020-08-02 15:26:02 -0300

    html + css for Map in detail

--- src/components/Detail/Visuals/Map.js
+++ src/components/Detail/Visuals/Map.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-export default function Map () {
+function formatAddress (displayAddress) {
+  let a = null
+
+  if (Array.isArray(displayAddress)) {
+    a = displayAddress.join(', ')
+  }
+
+  return a
+}
+
+export default function Map ({ coordinates, location }) {
+  console.log(coordinates, location)
+
+  const address = formatAddress(location.display_address)
+
   return (
     <div className='map-container'>
-      <div className='map'>Map</div>
+      <div className='map'>
+        Render MAP here
+        {address && <div className='address'>{address}</div>}
+        <div className='coords'>
+          Coords: {coordinates.latitude.toFixed(2)},
+          {coordinates.longitude.toFixed(2)}
+        </div>
+      </div>
     </div>
   )
 }
