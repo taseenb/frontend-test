4c96e751302515b2fdcd9fbf87a6bfbf3907b3bd
taseenb <esteban.almiron@gmail.com>
2020-08-02 21:53:38 -0300

    added all Yelp categories from simplified json

--- src/components/List/Filters/Category.js
+++ src/components/List/Filters/Category.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import Select from '../../shared/Select'
 
-// import data from '../../../__mocks__/categories.json'
-// const restaurants = data.categories.filter(cat => cat.parent_aliases[0] === 'restaurants')
-// console.log(restaurants)
+import restaurants from './categories.json'
 
+// Get options array from Yelp categories json
 const defaultOptions = [
-  { value: '', copy: 'All', selected: true, default: true },
-  { value: 'italian', copy: 'Italian', selected: false },
-  { value: 'seafood', copy: 'Seafood', selected: false },
-  { value: 'steak', copy: 'Steakhouses', selected: false },
-  { value: 'japanese', copy: 'Japanese', selected: false },
-  { value: 'tradamerican,newamerican', copy: 'American', selected: false },
-  { value: 'mexican', copy: 'Mexican', selected: false },
-  { value: 'thai', copy: 'Thai', selected: false }
-]
+  { value: '', copy: 'All', selected: true, default: true }
+].concat(
+  restaurants.map(r => {
+    return { value: r.alias, copy: r.title, selected: false }
+  })
+)
 
 function Category ({ selected, onChange }) {
   const filteredOptions = defaultOptions.map(option => {
