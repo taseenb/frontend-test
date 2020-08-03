ee6e55e3425eb7c98f6cba0f2be54bf6bdc93513
taseenb <esteban.almiron@gmail.com>
2020-08-03 01:00:37 -0300

    serve index.html from any route (fix)

--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
 require('dotenv').config()
 const config = require('./config')
+const debug = require('debug')
+const compression = require('compression')
 const express = require('express')
 const path = require('path')
 const apiRouter = require('./api-router')
 
+debug('server:express')
+
 const app = express()
+
+app.use(compression())
+
+// Serve all static files
+app.use(express.static(path.resolve(__dirname, '../dist')))
 
 // Serve API (Yelp API proxy)
 app.use('/api', apiRouter)
 
-// Serve static files
-app.use(express.static(path.resolve(__dirname, '../dist')))
+// Serve index.html from any route
+app.use('*', (req, res) => {
+  const indexHtml = path.resolve(__dirname, '../dist/index.html')
+  res.sendFile(indexHtml)
+})
 
 // Note: routes (including 404) are managed by React Router in the client
 
