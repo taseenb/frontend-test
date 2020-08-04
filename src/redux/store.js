0373d55730d352554acb2dd1232e5723bf791fb8
taseenb <esteban.almiron@gmail.com>
2020-08-04 16:59:42 -0300

    store updated with new is-prod global

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './root.reducer'
 
-const middlewares = IS_PROD ? [] : [logger]
+const middlewares = __IS_PROD__ ? [] : [logger]
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
